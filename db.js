35c882598c60d937c82df09f22a907555152787b
Manoj <manojb6153@gmail.com>
2025-03-13 11:19:07 +0530

    done

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/marketplace', {
+mongoose.connect('mongodb+srv://manojb6153:Manoj6153*@class0.oq4l6.mongodb.net/?retryWrites=true&w=majority&appName=Class0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
+}).then(() => console.log('MongoDB Connected to Atlas'))
   .catch(err => console.log(err));
 
 module.exports = mongoose;
